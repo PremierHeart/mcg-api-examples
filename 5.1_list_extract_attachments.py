d8672b6f6c0b02f8fe00d9332cd139b946ff513d
efedel <firstinitialastname@redacted.com>
2024-04-12 14:36:48 -0400

    added 5.1

--- 5.1_list_extract_attachments.py
+++ 5.1_list_extract_attachments.py
@@ -5,6 +5,34 @@ import sys
 import json
 import base64
 
+def retrieve_attachment(att):
+    # retrieve data for attachment
+    data = att['data']
+    if att['encoding'] == 'base64':
+        # att['data'] is a base64-encoded object such as a binary file
+        data = base64.b64decode(data)
+    if att['mime-type'] == 'application/json':
+        # att['data'] is a string containing JSON
+        data = json.loads(data)
+    # otherwise, att['data'] is inline-json and is already unpacked
+    return data
+
+def write_attachment_to_disk(att):
+    # write attachment to file on disk
+    output_data = retrieve_attachment(att)
+    mode = "wb"
+    if (att['mime-type'] == 'application/x-java-object') or \
+       (att['mime-type'] == 'application/json'):
+        # This is an inline JSON object and must be serialized
+        output_data = json.dumps(output_data)
+        mode = "w"
+
+    # NOTE: This does not determine filename extension
+    fname = + att['name'] + '.dat'
+    print("Writing " + fname + '...')
+    with open(fname, mode) as f: 
+        f.write(output_data)
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         raise "Missing AnalysisResults.json argument!"
@@ -19,23 +47,9 @@ if __name__ == '__main__':
             att['group'], att['input'],
             att['mime-type'], att['encoding']))
 
-        # retrieve data for attachment
-        data = att['data']
-        if att['encoding'] == 'base64':
-            # att['data'] is a base64-encoded object such as a binary file
-            data = base64.b64decode(data)
-        if att['mime-type'] == 'application/json':
-            # att['data'] is a string containing JSON
-            data = json.loads(data)
-        # at this point, the program can display or operate on 'data'
+        data = retrieve_attachment(att)
+        raw_cls = att['data'].__class__.__name__
+        xlat_cls =  data.__class__.__name__
+        print("     Data of class %s -> class %s" % (raw_cls, xlat_cls))
 
-        # write attachment to file on disk
-        output_data = att['data']
-        if att['mime-type'] == 'application/x-java-object':
-            # This is an inline JSON object and must be serialized
-            output_data = json.dumps(att['data'])
-        # NOTE: This does not determine filename extension
-        fname = att['name'] + '.dat'
-        print("Writing " + fname + '...')
-        with open(fname, 'w') as f: 
-            f.write(output_data)
+        write_attachment_to_disk(att)
